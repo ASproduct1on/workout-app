0904ace13c84bdfec5b94b3a036226d5cfa96f07
Andrii Skega <Game.a.s.production@gmail.com>
2023-11-07 02:37:48 +0100

    fix: fix arrow back button

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -16,24 +16,28 @@ const Header = ({ backLink = '' }) => {
 
   return (
     <header className={styles.header}>
-      {pathname !== '/' || pathname !== '/auth' || !isAuth ? (
-        <button
-          onClick={() => {
-            navigate(isAuth ? backLink : '/auth');
-          }}
-        >
-          <IoMdArrowBack fill="#fff" fontSize={29} />
-        </button>
-      ) : (
-        <button
-          onClick={() => {
-            navigate('/profile');
-          }}
-        >
-          <SlUser fill="#fff" fontSize={25} />
-        </button>
+      {isAuth && (
+        <>
+          {pathname === '/' && isAuth ? (
+            <button
+              onClick={() => {
+                navigate('/profile');
+              }}
+            >
+              <SlUser fill="#fff" fontSize={25} />
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                navigate(isAuth ? backLink : '/auth');
+              }}
+            >
+              <IoMdArrowBack fill="#fff" fontSize={29} />
+            </button>
+          )}
+          <Hamburger />
+        </>
       )}
-      {isAuth && <Hamburger />}
     </header>
   );
 };
